7b535174ca641893bb9fa02420d1b67bfb56fcc7
Tristan Berthiaume <tristan.007@live.ca>
2023-09-18 14:21:53 -0400

    Update Contacts_API.js

--- Contacts-Manager-SPA/js/Contacts_API.js
+++ Contacts-Manager-SPA/js/Contacts_API.js
@@ -1,6 +1,6 @@
 class Contacts_API {
     // static API_URL() { return "http://localhost:5000/api/contacts" };
-   static API_URL() { return "https://pepper-versed-jet.glitch.me/api/contacts" };
+   static API_URL() { return "../Contacts-API/contacts.json" };
     static async Get(id = null) {
         return new Promise(resolve => {
             $.ajax({
