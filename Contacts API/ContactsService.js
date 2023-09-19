d36ee6e475cf54b043b51957330fc58c65b35e27
Tristan Berthiaume <tristan.007@live.ca>
2023-09-18 21:20:21 -0400

    x

--- Contacts API/ContactsService.js
+++ Contacts API/ContactsService.js
@@ -27,9 +27,9 @@ function response(res, status, data = null) {
 }
 async function handleContactsRequest(req, res) {
     console.log("YESYESYESYESYESYESYESYESYESYESYES");
-    let contactsRepository = new Repository("./bookmarks.json");
+    let contactsRepository = new Repository("./contacts.json");
     let contact = null;
-    if (req.url == "/api/bookmarks") {
+    if (req.url == "/api/contacts") {
         switch (req.method) {
             case "GET":
                 return response(res, 200, JSON.stringify(contactsRepository.getAll()));
